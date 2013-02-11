153ef1a2fbfa56b84b6c73f2f78d15adf914ef07
kit <308tube@gmail.com>
2013-02-10 21:16:51 -0500

    new database status resource
    Created a new api resouce that will return the Oracle date/time stamp.
    Can be used to ensure database connection is good.

--- src/com/youtube/rest/status/V1_status.java
+++ src/com/youtube/rest/status/V1_status.java
@@ -1,7 +1,11 @@
 package com.youtube.rest.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+
+import java.sql.*;
+
+import com.youtube.dao.*;
 
 /**
  * This is the root path for our restful api service
@@ -18,7 +22,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/v1/status/*")
 public class V1_status {
 
-	private static final String api_version = "00.01.00"; //version of the api
+	private static final String api_version = "00.02.00"; //version of the api
 	
 	/**
 	 * This method sits at the root of the api.  It will return the name
@@ -47,6 +51,60 @@ public class V1_status {
 	@Produces(MediaType.TEXT_HTML)
 	public String returnVersion() {
 		return "<p>Version:</p>" + api_version;
+	}
+	
+	/**
+	 * This method will connect to the oracle database and return the date/time stamp.
+	 * It will then return the date/time to the user in String format
+	 * 
+	 * This was explained in Part 3 of the Java Rest Tutorial Series on YouTube
+	 * 
+	 * @return String -  returns the database date/time stamp
+	 * @throws Exception
+	 */
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception {
+		
+		PreparedStatement query = null;
+		String myString = null;
+		String returnString = null;
+		Connection conn = null;
+		
+		try {
+			
+			conn = Oracle308tube.Oracle308tubeConn().getConnection(); //calls the method defined in the Oracle308tube package
+			
+			//simple sql query to return the date/time
+			query = conn.prepareStatement("select to_char(sysdate,'YYYY-MM-DD HH24:MI:SS') DATETIME " +
+					"from sys.dual");
+			ResultSet rs = query.executeQuery();
+			
+			//loops through the results and save it into myString
+			while (rs.next()) {
+				// /*Debug*/ System.out.println(rs.getString("DATETIME"));
+				myString = rs.getString("DATETIME");
+			}
+			
+			query.close(); //close connection
+			
+			returnString = "<p>Database Status</p> " +
+				"<p>Database Date/Time return: " + myString + "</p>";
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		/**
+		 * The finally cause will always run. Even if the the method get a error.
+		 * You want to make sure the connection to the database is closed.
+		 */
+		finally {
+			if (conn != null) conn.close();
+		}
+		
+		return returnString; 
 	}
 	
 }
