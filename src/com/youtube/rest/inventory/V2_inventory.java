7ab9a5c5db6ad6513f328806cdb3674c5edd7aa2
kit <308tube@gmail.com>
2013-06-30 20:02:19 -0400

    added example of pathparam
    added example of pathpararm annotation and also add simple error method
    in case user hits the room of inventory.

--- src/com/youtube/rest/inventory/V2_inventory.java
+++ src/com/youtube/rest/inventory/V2_inventory.java
@@ -2,6 +2,7 @@ package com.youtube.rest.inventory;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -43,4 +44,38 @@ public class V2_inventory {
 		return Response.ok(returnString).build();
 	}
 	
+	/*
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnErrorOnBrand() throws Exception {
+		
+		return Response.status(400).entity("Error: please specify brand for this search").build();
+	}
+	*/
+	
+	@Path("/{brand}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrand(
+				@PathParam("brand") String brand) 
+				throws Exception {
+		
+		String returnString = null;
+		
+		JSONArray json = new JSONArray();
+		
+		try {
+			
+			Schema308tube dao = new Schema308tube();
+			
+			json = dao.queryReturnBrandParts(brand);
+			returnString = json.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
 }
