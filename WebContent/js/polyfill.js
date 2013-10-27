55741c47819fb7ad65196ad7be1c9eb3df392394
kit <308tube@gmail.com>
2013-10-26 23:31:41 -0400

    Added html/js for post ep6 POST video
    adding in html and javascript file needed to demo post api.  This is
    done before the start of episode 6.

---
+++ WebContent/js/polyfill.js
@@ -0,0 +1,23 @@
+/**
+ * jquery plugin
+ * Serialize a form to javascript object
+ * @returns js object
+ */
+(function($) {
+	$.fn.serializeObject = function()
+	{
+		var o = {};
+		var a = this.serializeArray();
+		$.each(a, function() {
+			if (o[this.name] !== undefined) {
+				if (!o[this.name].push) {
+					o[this.name] = [o[this.name]];
+				}
+				o[this.name].push(this.value || '');
+			} else {
+				o[this.name] = this.value || '';
+			}
+		});
+		return o;
+	};
+})(jQuery);
---
+++ WebContent/js/post.js
@@ -0,0 +1,87 @@
+/**
+ * js file for post.html
+ * Please use modern web browser as this file will not attempt to be
+ * compatible with older browsers. Use Chrome and open javascript console
+ * or Firefox with developer console.
+ * 
+ * jquery is required
+ */
+$(document).ready(function() {
+	//console.log("ready");
+	
+	var $post_example = $('#post_example');
+	
+	/**
+	 * This is for the Submit button
+	 * It will trigger a ajax POST call to: api/v2/inventory
+	 * This will submit a item entry to our inventory database
+	 */
+	$('#submit_it').click(function(e) {
+		//console.log("submit button has been clicked");
+		e.preventDefault(); //cancel form submit
+		
+		var jsObj = $post_example.serializeObject()
+			, ajaxObj = {};
+		
+		//console.log(jsObj);
+		
+		ajaxObj = {  
+			type: "POST",
+			url: "http://localhost:7001/com.youtube.rest/api/v2/inventory/", 
+			data: JSON.stringify(jsObj), 
+			contentType:"application/json",
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("Error " + jqXHR.getAllResponseHeaders() + " " + errorThrown);
+			},
+			success: function(data) { 
+				//console.log(data);
+				if(data[0].HTTP_CODE == 200) {
+					$('#div_ajaxResponse').text( data[0].MSG );
+				}
+			},
+			complete: function(XMLHttpRequest) {
+				//console.log( XMLHttpRequest.getAllResponseHeaders() );
+			}, 
+			dataType: "json" //request JSON
+		};
+		
+		$.ajax(ajaxObj);
+	});
+	
+	/**
+	 * This is for the 2nd Submit button "Submit v2"
+	 * It will do the same thing as Submit above but the api
+	 * will process it in a different way.
+	 */
+	$('#submit_it2').click(function(e) {
+		//console.log("submit button has been clicked");
+		e.preventDefault(); //cancel form submit
+		
+		var jsObj = $post_example.serializeObject()
+			, ajaxObj = {};
+		
+		//console.log(jsObj);
+		
+		ajaxObj = {  
+			type: "POST",
+			url: "http://localhost:7001/com.youtube.rest/api/v2/inventory/v2", 
+			data: JSON.stringify(jsObj), 
+			contentType:"application/json",
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("Error " + jqXHR.getAllResponseHeaders() + " " + errorThrown);
+			},
+			success: function(data) { 
+				//console.log(data);
+				if(data[0].HTTP_CODE == 200) {
+					$('#div_ajaxResponse').text( data[0].MSG );
+				}
+			},
+			complete: function(XMLHttpRequest) {
+				//console.log( XMLHttpRequest.getAllResponseHeaders() );
+			}, 
+			dataType: "json" //request JSON
+		};
+		
+		$.ajax(ajaxObj);
+	});
+});
